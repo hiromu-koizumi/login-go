c0678094b09a8bf69db32c0ea41f0b1a9912556f
小泉大夢 <abn21029@gmail.com>
2020-03-22 17:15:14 +0900

    featch db_info

--- docker/mysql/init/create_db.sql
+++ docker/mysql/init/create_db.sql
@@ -1,3 +1,10 @@
 SET CHARSET UTF8;
 DROP DATABASE IF EXISTS sample_docker_compose;
 CREATE DATABASE IF NOT EXISTS sample_docker_compose DEFAULT CHARACTER SET utf8;
+use sample_docker_compose;
+CREATE TABLE users (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name TEXT NOT NULL
+)DEFAULT CHARACTER SET=utf8;
+ 
+INSERT INTO users (name) VALUES ("太郎"),("花子"),("令和");
--- main.go
+++ main.go
@@ -1,13 +1,47 @@
 package main
 
-import(
+import (
+	"database/sql"
 	"fmt"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type User struct {
+	ID   int
+	Name string
+}
 
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/sample_docker_compose")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(user.ID, user.Name)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		panic(err.Error())
+	}
+
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
